3a37abff0230249472c1fe2bbd4ca8a6a4523967
Redian Marku <42848358+redianmarku@users.noreply.github.com>
2020-06-15 11:43:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,9 @@ session.set_do_follow(True, percentage=50, times=1)
 session.like_by_tags(['javascript', 'python'], amount=3)
 session.set_dont_like(["naked", "nsfw", "sex"])
 
-
-
-# #comment section
-# session.set_do_comment(True, percentage=100)
-# session.set_comments([u'I love your post @{}!'])
+#comment section
+session.set_do_comment(True, percentage=100)
+session.set_comments([u'I love your post @{}!'])
 
 
 session.end()
